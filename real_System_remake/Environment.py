fc0cbb315b4fc17359c74b790aa2316e3e94afec
oneplus <1509295051@qq.com>
2026-03-17 14:59:35 +0800

    重复验证，只修改Environment.py

--- real_System_remake/Environment.py
+++ real_System_remake/Environment.py
@@ -52,7 +52,7 @@ class Environment:
         if self.use_swanlab:
             swanlab.init(project="cortex24_oneplus",
                          name=name,
-                         notes="增加预训练代码，直接使用生成器生成企业动作",
+                         notes="重复验证",
                          config=swanlab_config)
         self.name = name
         self.lim_day = lim_day  # 设置的生存时间上限，如果要改的话在system.py的self.env = Environment(name='TD3_1_3', lim_day=100)中改就好了
