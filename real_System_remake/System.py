0dd967d2a83787a47ad5da41a0078dc91dee62b0
oneplus <1509295051@qq.com>
2026-06-11 08:33:31 +0800

    online GAIL+TD3 v1.21, paper profit curves, bank reward curve, and survival logging beyond step 60

--- real_System_remake/System.py
+++ real_System_remake/System.py
@@ -322,8 +322,8 @@ class System:
 
 
 if __name__ == '__main__':
-    # seeds_to_run = [184, 291, 83, 739, 512, 117, 894, 652]
-    seeds_to_run = [184, 291, 83, 739]
+    seeds_to_run = [184, 291, 83, 739, 512, 117, 894, 652]
+    # seeds_to_run = [184, 291, 83, 739]
     for seed in seeds_to_run:
         system = System(seed=seed)
         system.run()
