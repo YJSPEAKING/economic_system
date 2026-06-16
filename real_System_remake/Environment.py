1c7bdc7fd6370c2262e6dc247ab9b8e5f29a49e8
oneplus <1509295051@qq.com>
2026-06-17 00:14:10 +0800

    online GAIL+TD3 v1.23, paper income curves and standard 100-episode survival average

--- real_System_remake/Environment.py
+++ real_System_remake/Environment.py
@@ -80,7 +80,7 @@ class Environment:
         if self.use_swanlab:
             swanlab.init(project="cortex24_oneplus",
                          name=name,
-                         notes="online GAIL+TD3 v1.21, paper income curves and survival top-50 average after step 14",
+                    notes="online GAIL+TD3 v1.23, paper income curves and standard 100-episode survival average",
                          config=swanlab_config)
         self.name = name
         self.lim_day = lim_day  # 设置的生存时间上限，如果要改的话在system.py的self.env = Environment(name='TD3_1_3', lim_day=100)中改就好了
